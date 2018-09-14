0708057db9384d17cd8c72cdaf8c54641a3357d4
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-14 18:31:35 +0530

    add

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("deviceready",onDeviceReady,false);
 	function onDeviceReady(){
 		alert("device is starting");
 		//document.getElementById("btn1").addEventListener("click",cameraTakePicture);
-		document.getElementById("btn2").addEventListener("click",scanBarCode);
+		//document.getElementById("btn2").addEventListener("click",scanBarCode);
 		cordova.plugins.barcodeScanner.encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
             alert("encode success: " + success);
           }, function(fail) {
@@ -44,4 +44,31 @@ document.addEventListener("deviceready",onDeviceReady,false);
 					   
 
 	}
-}
+}var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
