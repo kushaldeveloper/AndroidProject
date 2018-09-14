7ddbd27172b720ade3a8359730741811930e63de
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-14 15:46:42 +0530

    adddd

--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,38 @@
 document.addEventListener("deviceready",onDeviceReady,false);
-    function onDeviceReady(){
-        alert("device is starting");
-        document.getElementById("btn1").addEventListener("click",cameraTakePicture);
-    function cameraTakePicture(){
-        alert("camera is starting");
-        //alert("camera :" +navigator.camera);
-        //alert("camera :"+ navigator.camera.getPicture);
-                        navigator.camera.getPicture(onSuccess, onFail, {  
-                          quality: 50, 
-                          destinationType: Camera.DestinationType.DATA_URL 
-                       }); 
-        }               
-                        function onSuccess(imageData) { 
-                          alert('sucess');
-                          var image = document.getElementById('myImage'); 
-                          image.src = "data:image/jpeg;base64," + imageData; 
-                       } 
-                        function onFail(message) { 
-                          alert('Failed because: ' + message); 
-                       } 
+	function onDeviceReady(){
+		alert("device is starting");
+		document.getElementById("btn1").addEventListener("click",cameraTakePicture);
+		document.getElementById("btn2").addEventListener("click",scanBarCode);
+	function cameraTakePicture(){
+		alert("camera is starting");
+		//alert("camera :" +navigator.camera);
+		//alert("camera :"+ navigator.camera.getPicture);
+						navigator.camera.getPicture(onSuccess, onFail, {  
+						  quality: 50, 
+						  destinationType: Camera.DestinationType.DATA_URL 
+					   }); 
+		}			    
+						function onSuccess(imageData) { 
+						  alert('sucess');
+						  var image = document.getElementById('myImage'); 
+						  image.src = "data:image/jpeg;base64," + imageData; 
+					   } 
+						function onFail(message) { 
+						  alert('Failed because: ' + message); 
+					   } 
+
+	/*function scanBarCode(){
+		alert('barcode scanner is starting');
+		cordova.plugins.barcodeScanner.scan(result,error);
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }
+  };
+      function (error) {
+          alert("Scanning failed: " + error);
+      }*/
+					   
 }
